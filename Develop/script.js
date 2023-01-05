aea24693b488d767754a19b7968f321f656aae12
MarcusShelle <117790381+MarcusShelle@users.noreply.github.com>
2023-01-04 22:00:21 -0500

    best starting point

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,43 @@
 // Assignment code here
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var lowerCase =[ 'a', 'b', ]
+
+
+function userInfo() {
+
+
+  var length = prompt("What would you like the length to be?")
+
+if (length < 8 || length > 128) {
+  alert("please enter a number between 8 and 128")
+  return null;
+}
+
+var isLowerCase = confirm("Would you like lower case characters?")
+console.log(typeof length);
+
+
+
+
+
+
+
+
+
+
+
+
+var userOptions = {
+  length: length, 
+  isLowerCase: isLowerCase,
+}
+  return userOptions;
+}
+
+function generatePassword() {
+  var userChoices = userInfo()
+
+}
 
 // Write password to the #password input
 function writePassword() {
